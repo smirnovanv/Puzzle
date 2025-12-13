4d598db99b10762479aa077db6223ec8658e0694
Nataly <smirnovanv89@gmail.com>
2025-12-13 23:29:37 +0300

    Base Functionality: Delete matched balls and make them fall

--- Assets/Scripts/BallData.cs
+++ Assets/Scripts/BallData.cs
@@ -6,9 +6,18 @@ public class BallData
     public int x;
     public int y;
     public int type;
-    // public bool isMatched = false;
+    public bool isMatched = false;
     public GameObject visualObject; // ссылка на визуальный объект
-
+    public BonusType bonus = BonusType.None;
+    public enum BonusType
+    {
+        None,
+        HorizontalRocket,   // Очищает всю строку
+        VerticalRocket,     // Очищает весь столбец
+        Bomb,              // Взрывает соседние шарики (3x3 область)
+        ColorBomb,         // Уничтожает все шарики одного цвета
+        Rainbow            // Специальный бонус (по желанию)
+    }
     public BallData(int x, int y, int type)
     {
         this.x = x;
